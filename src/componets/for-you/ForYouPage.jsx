1bbfba8dfd1ed1813807c3ec200a1a3db3470a54
Osezua Ikekhua <osezuaikekhua@gmail.com>
2023-12-27 14:02:57 -0600

    Finished for you page. mapped the api data on the page.

--- src/componets/for-you/ForYouPage.jsx
+++ src/componets/for-you/ForYouPage.jsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { IoMdPlayCircle } from "react-icons/io";
-
+import { CiClock2 } from "react-icons/ci";
+import { IoIosStarOutline } from "react-icons/io";
 
 function ForYouPage() {
     const[selectedBooks, setSelectedBooks] = useState([])
+    const[recommendedBooks, setrecommendedBooks] = useState([])
+    const[suggestedBooks, setSuggestedBooks] = useState([])
 
-    async function getBooks(api){
+    async function getBooks(constVal, api){
       const { data } = await axios.get(api)
-      setSelectedBooks(data)
-      console.log(selectedBooks)
+      constVal(data)
+      console.log(suggestedBooks)
     }
 
     useEffect(() => {
-      getBooks('https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected')
+      getBooks(setSelectedBooks, 'https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected')
+      getBooks(setrecommendedBooks, "https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended")
+      getBooks(setSuggestedBooks, "https://us-central1-summaristt.cloudfunctions.net/getBooks?status=suggested")
     },[])
 
   return (
@@ -25,7 +30,7 @@ function ForYouPage() {
             
             {
               selectedBooks.map(book => (
-                <div className='Selected__Book__Section'>
+                <div className='Selected__Book__Section' >
                   <p>{book.subTitle}</p>
                   <div className='line'></div>
                   <div className='Selected__Book__area'>
@@ -39,6 +44,52 @@ function ForYouPage() {
                 </div>
               ))}
         </section>
+
+        <section className="Recommended__Books__Section">
+            <h2>Recommended For You</h2>
+            <p>We think you'll like these</p>
+
+            <div className="Recommended__Books">
+                {
+                  recommendedBooks.slice(0,4).map(book => (
+                    <div className="Book">
+                      <img className='Book__image' src={book.imageLink} alt=""/>
+                      <div className="Book__Title">{book.title}</div>
+                      <div className="Book__authorName">{book.author}</div>
+                      <div className="Book__Description">{book.subTitle}</div>
+                      <footer>
+                        <div className="Book__Length"> <CiClock2 /> 03:24</div>
+                        <div className="Book__Rating"> <IoIosStarOutline /> {book.averageRating}</div>
+                      </footer>
+                    </div>
+                ))}
+                
+                
+
+            </div>
+        </section>
+
+        <section className="Recommended__Books__Section">
+            <h2>Suggested Books</h2>
+            <p>We think you'll like these</p>
+
+            <div className="Recommended__Books">
+                {
+                  suggestedBooks.slice(0,4).map(book => (
+                    <div className="Book">
+                      <img className='Book__image' src={book.imageLink} alt=""/>
+                      <div className="Book__Title">{book.title}</div>
+                      <div className="Book__authorName">{book.author}</div>
+                      <div className="Book__Description">{book.subTitle}</div>
+                      <footer>
+                        <div className="Book__Length"> <CiClock2 /> 03:24</div>
+                        <div className="Book__Rating"> <IoIosStarOutline /> {book.averageRating}</div>
+                      </footer>
+                    </div>
+                ))}
+              </div>
+        </section>
+
 
       </div>
     
