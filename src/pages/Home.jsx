abfb78e96d2138436e21a310fb9982fd1e4edabb
Osezua Ikekhua <osezuaikekhua@gmail.com>
2024-03-08 11:31:11 -0600

    Started on Choose Plan Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,109 +1,28 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+
 
 import Nav from '../componets/home/Nav'
 import Landing from '../componets/home/Landing'
 import Features from '../componets/home/Features'
 import Reviews from '../componets/home/Reviews'
 import Numbers from '../componets/home/Numbers'
-import Modual from '../componets/Modules/Modual'
+import Footer from '../componets/home/Footer'
 
 import logo from '../images/logo.png'
 import landing from '../images/landing.png'
+
 
 function Home({ showModule}) {
  
   return (
     <>
-    
     <Nav LogoImg = {logo} showModule={showModule} />
     <Landing LandingImg = {landing} showModule = {showModule} />
     <Features />
     <Reviews showModule = {showModule} />
     <Numbers />
-    
-    <section id="footer">
-      <div class="container">
-        <div class="row">
-          <div class="footer__top--wrapper">
-            <div class="footer__block">
-              <div class="footer__link--title" >Action</div>
-              <div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Summarist Magazine</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Cancel Subscription</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Help</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Contact us</a>
-                </div>
-              </div>
-            </div>
-            <div class="footer__block">
-              <div class="footer__link--title">Useful Links</div>
-              <div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Pricing</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Summarist Business</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Gift Cards</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Authors & Publishers</a>
-                </div>
-              </div>
-            </div>
-            <div class="footer__block">
-              <div class="footer__link--title">Company</div>
-              <div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">About</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Careers</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Partners</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Code of Conduct</a>
-                </div>
-              </div>
-            </div>
-            <div class="footer__block">
-              <div class="footer__link--title">Other</div>
-              <div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Sitemap</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Legal Notice</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Terms of Service</a>
-                </div>
-                <div class="footer__link--wrapper">
-                  <a class="footer__link">Privacy Policies</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="footer__copyright--wrapper">
-            <div class="footer__copyright">
-              Copyright &copy; 2023 Summarist.
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Footer />
+
     </>
   )
 }
